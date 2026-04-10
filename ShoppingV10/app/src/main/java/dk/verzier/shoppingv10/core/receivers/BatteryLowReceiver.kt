90f1a6442c3ff9fc65b6ec0b144a06ea9f4d2cf3
shichenitu <shichenitu@163.com>
2026-04-10 19:57:42 +0200

    ShoppingV10-function-batterylow-notify

--- ShoppingV10/app/src/main/java/dk/verzier/shoppingv10/core/receivers/BatteryLowReceiver.kt
+++ ShoppingV10/app/src/main/java/dk/verzier/shoppingv10/core/receivers/BatteryLowReceiver.kt
@@ -12,5 +12,20 @@ class BatteryLowReceiver : BroadcastReceiver() {
         // TODO Trigger notification when battery is low
         // HINT: Use NotificationHelper.showNotification() with a unique static ID for this specific
         // notification & check strings.xml for the notification title and message.
+        if (intent.action == Intent.ACTION_BATTERY_LOW) {
+            Log.d("BatteryReceiver", "Warn of Low Battery, ready to notify")
+
+            val title = context.getString(R.string.battery_low_title)
+            val message = context.getString(R.string.battery_low_message)
+
+            val batteryNotificationId = 999
+
+            NotificationHelper.showNotification(
+                context = context,
+                title = title,
+                message = message,
+                notificationId = batteryNotificationId
+            )
+        }
     }
 }
